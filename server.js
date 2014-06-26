2bcccaa51ba4a386b6fbfe4a922c8d5aa9f3bccc
NQB <notquitebeta@ymail.com>
2014-06-25 21:25:01 -0400

    Added date/time REST call and date functions to server.js.  Added ajax call and Jquery html to script.js.

--- server.js
+++ server.js
@@ -23,6 +23,91 @@ app.get('/', function(req,res){
   res.render('index.html');
 });
 
+app.get('/time', function(req,res){
+  formatDate(res);
+});
+
 app.listen(port, function(){
   console.log('running '+appName+' on port '+port+'.');
 });
+
+function formatDate(res) {
+    var now = new Date(),
+        weekday = [],
+        month = [];
+    weekday[0]=  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    var dHash = {};
+    dHash["toString"] = now.toString();
+    dHash["getDate"] = now.getDate();
+    dHash["getDay"] = now.getDay();
+    dHash["getDayOfTheWeek"] = weekday[now.getDay()];
+    dHash["getFullYear"] = now.getFullYear();
+    dHash["getHours"] = now.getHours();
+    dHash["getMilliseconds"] = now.getMilliseconds();
+    dHash["getMinutes"] = now.getMinutes();
+    dHash["getMonth"] = now.getMonth();
+    dHash["getMonthOfTheYear"] = month[now.getMonth()];
+    dHash["getSeconds"] = now.getSeconds();
+    dHash["getTime"] = now.getTime();
+    dHash["getTimezoneOffset"] = now.getTimezoneOffset();
+    dHash["getUTCDate"] = now.getUTCDate();
+    dHash["getUTCDay"] = now.getUTCDay();
+    dHash["getUTCFullYear"] = now.getUTCFullYear();
+    dHash["getUTCHours"] = now.getUTCHours();
+    dHash["getUTCMilliseconds"] = now.getUTCMilliseconds();
+    dHash["getUTCMinutes"] = now.getUTCMinutes();
+    dHash["getUTCMonth"] = now.getUTCMonth();
+    dHash["getUTCSeconds"] = now.getUTCSeconds();
+    dHash["getYear"] = now.getYear();
+    // dHash["setDate"] = now.setDate();
+    // dHash["setFullYear"] = now.setFullYear();
+    // dHash["setHours"] = now.setHours();
+    // dHash["setMilliseconds"] = now.setMilliseconds();
+    // dHash["setMinutes"] = now.setMinutes();
+    // dHash["setMonth"] = now.setMonth();
+    // dHash["setSeconds"] = now.setSeconds();
+    // dHash["setTime"] = now.setTime();
+    // dHash["setUTCDate"] = now.setUTCDate();
+    // dHash["setUTCFullYear"] = now.setUTCFullYear();
+    // dHash["setUTCHours"] = now.setUTCHours();
+    // dHash["setUTCMilliseconds"] = now.setUTCMilliseconds();
+    // dHash["setUTCMinutes"] = now.setUTCMinutes();
+    // dHash["setUTCMonth"] = now.setUTCMonth();
+    // dHash["setUTCSeconds"] = now.setUTCSeconds();
+    // dHash["setYear"] = now.setYear();
+    // dHash["toDateString"] = now.toDateString();
+    // dHash["toGMTString"] = now.toGMTString();
+    // // dHash["toISOString"] = now.toISOString();
+    // dHash["toJSON"] = now.toJSON();
+    // dHash["toLocaleDateString"] = now.toLocaleDateString();
+    // // dHash["toLocaleFormat"] = now.toLocaleFormat();
+    // dHash["toLocaleString"] = now.toLocaleString();
+    // dHash["toLocaleTimeString"] = now.toLocaleTimeString();
+    // // dHash["toSource"] = now.toSource();
+    // dHash["toString"] = now.toString();
+    // dHash["toTimeString"] = now.toTimeString();
+    // dHash["toUTCString"] = now.toUTCString();
+    // dHash["valueOf"] = now.valueOf();
+//  " + y + " : now' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+    res.jsonp(dHash);
+}
--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,4 +1,55 @@
 $(document).ready(function(){
-
+  getDateInfo();
 
 });
+
+function getDateInfo(){
+    var dateField = $(".dateInfo");
+
+    $.ajax({
+    url: "/time",
+    type:'GET',
+    dataType: 'jsonp',
+    success: function(res) {
+      // console.log(res)
+      s = "";
+      s += res["getDayOfTheWeek"];
+      s += ", ";
+      s += res["getMonthOfTheYear"];
+      s += " ";
+      s += res["getDate"];
+      s += ", ";
+      s += res["getFullYear"];
+      s += ". ";
+      var ampm;
+      var hour = res["getHours"];
+      if (hour == 0){
+        s += "12";
+        ampm = "AM";
+      }else if (hour < 13){
+        s += hour;
+        ampm = "AM";
+      }else {
+        s += hour - 12;
+        ampm = "PM";
+      }
+      s += ":";
+      s += ("00"+res["getMinutes"]).slice(-2);
+      s += ":";
+      s += ("00"+res["getSeconds"]).slice(-2);
+      s += " ";
+      s += ampm;
+      s += ".";
+
+
+      // s += "<pre>"+JSON.stringify(res['coord'], null, 4)+"</pre>";
+      // s += "<pre>"+JSON.stringify(res, null, 4)+"</pre>";
+       
+      dateField.html(s);
+
+      dateField.on('click', function(){
+         getDateInfo();
+      });
+    }
+  });
+}
